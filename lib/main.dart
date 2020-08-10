12119194360e65b82dd29f5d7bfb9d66897d7544
Ana Luisa <ana.bavati@nubank.com.br>
2020-08-10 02:18:59 -0300

    day 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:bytebank/screens/transfer/form.dart';
-import 'package:bytebank/screens/transfer/list.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(ByteBankApp());
@@ -7,19 +5,65 @@ void main() => runApp(ByteBankApp());
 class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var transferFormWidget = TransferForm();
-
     return MaterialApp(
-      home: Scaffold(
-        body: TransferList(),
+        theme: ThemeData(
+          primaryColor: Colors.green[900],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
+          ),
+        ),
+        home: Dashboard());
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
       ),
-      theme: ThemeData(
-        primaryColor: Colors.red[900],
-        accentColor: Colors.red[700],
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.red[200],
-          textTheme: ButtonTextTheme.primary,
-        )
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/bytebank_logo.png')),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 100,
+              width: 150,
+              color: Theme.of(context).primaryColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  Text(
+                    'Contacts',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
